f537fcaca5bdbf143f415faafe232416792846ee
Swakiro <151346678+FatornahBk@users.noreply.github.com>
2026-05-25 21:41:36 +0700

    feat(admin): initialize admin pages layouts and services folders

---
+++ Front-end/src/layouts/AdminLayout.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+
+import Navbar from "../components/navbar";
+
+function AdminLayout() {
+    return (
+        <div className="flex flex-col min-h-screen">
+            <Navbar />
+            <main className="flex-1">
+                <Outlet />
+            </main>
+        </div>
+    );
+}
+
+export default AdminLayout;
